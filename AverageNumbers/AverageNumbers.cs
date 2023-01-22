d1096539b11d4dcc88e7bce2228f3bb17aafde38
Nadezhda Traykova <n.l.traykova@gmail.com>
2023-01-22 22:28:45 +0200

    Add AverageNumbers Project

---
+++ AverageNumbers/AverageNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageNumbers
+{
+    //Method for average of array of numbers
+    public static double Average(int[] arr)
+    {
+        double sum = 0.0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum / arr.Length;
+    
+    }
+
+
+}
